5152e6479648908afdd3259d680ee09792bf3442
Alastair Coote <alastair.coote@gmail.com>
2013-08-06 18:24:11 -0400

    Initial stuff

---
+++ app/scripts/main.js
@@ -0,0 +1,23 @@
+require.config({
+    paths: {
+        jquery: '../bower_components/jquery/jquery',
+        backbone: '../bower_components/backbone/backbone',
+        underscore: '../bower_components/underscore/underscore'
+    },
+    shim: {
+        'backbone': {
+            deps: ['underscore', 'jquery'],
+            exports: 'Backbone'
+        },
+        'underscore': {
+            exports: '_'
+        }
+    }
+});
+
+require(['app', 'jquery'], function (app, $) {
+    'use strict';
+    // use app here
+    console.log(app);
+    console.log('Running jQuery %s', $().jquery);
+});
